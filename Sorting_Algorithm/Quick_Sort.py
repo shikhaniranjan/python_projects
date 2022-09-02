10f357fcd7a9c36facfb8d67fe36d68923e1d236
shniranj <shniranj@cisco.com>
2022-09-02 21:12:51 +0530

    n

---
+++ Sorting_Algorithm/Quick_Sort.py
@@ -0,0 +1,31 @@
+class QuickSort():
+    def partition(self,l,low,high):
+        i= -1
+        piv = l[high]
+        for j in range(0, high):
+            if l[j] < piv:
+                i += 1
+                # swap
+                temp = l[j]
+                l[j] = l[i]
+                l[i] = temp
+        i += 1
+        # swap last
+        temp = l[i]
+        l[i] = l[high]
+        l[high] = temp
+
+        return i
+    def quick_sort(self,l,low,high):
+        if low <high:
+            piv_index = self.partition(l,low,high)
+            self.quick_sort(l,low,piv_index-1)
+            self.quick_sort(l,piv_index+1,high)
+        return l
+
+
+l = [6,9,10,2,8]
+QS = QuickSort()
+n =len(l)
+sorted_list = QS.quick_sort(l,0,n-1)
+print(sorted_list)
--- leet_code/Remove_element from_list.py
+++ leet_code/Remove_element from_list.py
@@ -1,7 +1,12 @@
 class Solution:
     def removeElement(self, nums, val):
+        from pdb import set_trace
+        set_trace()
         nums = [ele for ele in nums if ele !=val]
-        return (len(nums))
+        return (nums)
 
 nums = [3,2,2,3]
 val = 3
+Sol =Solution()
+res = Sol.removeElement(nums,val)
+print(res)
